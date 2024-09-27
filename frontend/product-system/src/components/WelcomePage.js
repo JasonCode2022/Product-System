122bbac0f7d8bc1befd02161b8a5dbe18af0fbc8
unknown <jason_ishac@hotmail.com>
2024-09-28 00:27:44 +0200

    Test Update

--- frontend/product-system/src/components/WelcomePage.js
+++ frontend/product-system/src/components/WelcomePage.js
@@ -6,7 +6,7 @@ function WelcomePage() {
     
     <div>
       
-      <h1>Welcome to My Application</h1>
+      <h1>Welcome to My Application!!!!!</h1>
       <p>This is the welcome page of my application.</p>
       
     </div>
